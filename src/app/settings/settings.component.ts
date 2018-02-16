4f5bf23ba6f4b760531ae829539e8082ab6f33cc
NA-JanP <pienkniewski@newsaktuell.de>
2018-02-16 18:29:37 +0100

    DB JSON mit News HTML und Beispiel HTML Sani

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -13,3 +13,42 @@ export class SettingsComponent implements OnInit {
   }
 
 }
+/*
+
+
+import { Component, Input, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+
+
+
+
+  constructor(
+    private sanitizer: DomSanitizer
+    ) {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+
+
+
+    Promise
+      .all(promises)
+      .then((values) => {
+        this.textFullSafe = this.sanitizer.bypassSecurityTrustHtml(values.join(''));
+        this.moreInfoClasses['pt-hidden'] = false;
+      })
+      .catch((e) => {
+        this.isLoading = false;
+        console.log('error while reading content blocks', e);
+      });
+
+  }
+
+
+
+
+
+
+* */
